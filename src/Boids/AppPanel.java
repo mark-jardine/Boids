d93d29edc3db9dc4ecf3672a2bafe976ef56d521
mark-jardine <markjardine27@gmail.com>
2022-02-14 17:53:27 +0000

    In the process of trying to get circles spawning in random locations and moving with velocities.

--- src/Boids/AppPanel.java
+++ src/Boids/AppPanel.java
@@ -2,13 +2,22 @@ package Boids;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.desktop.ScreenSleepEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+import java.util.Timer;
 
 
 public class AppPanel extends JPanel implements ActionListener {
     public final static int PANEL_WIDTH = 1100;
     public final static int PANEL_HEIGHT = 750;
+    public final static int NUM_BOIDS = 10;
+    ArrayList<Boid> boids = Boid.spawn(NUM_BOIDS);
+    Timer timer;
+    boolean running = false;
+
 
     public AppPanel() {
         this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
@@ -17,19 +26,32 @@ public class AppPanel extends JPanel implements ActionListener {
 
     @Override
     public void paintComponent(Graphics g) {
+//        super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
         RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING
                 , RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHints(hints);
+        //g2d.draw(new Ellipse2D.Double(20,20,30,30));
 
+
+        try {
+            for (Boid b : boids) {
+                Ellipse2D.Double boidShape = new Ellipse2D.Double(b.getxPos(), b.getyPos(), 30, 30);
+                g2d.draw(boidShape);
+                g2d.fill(boidShape);
+                b.move();
+            }
+        } catch (NullPointerException n) {
+            System.out.println(n.getMessage());
+        }
+        repaint();
     }
 
-    public void draw(Graphics g) {
 
-    }
-
-    private void init_Boids(){
+    private void init_Sim() {
+        running = true;
+        timer = new Timer();
 
 
     }
--- src/Boids/Boid.java
+++ src/Boids/Boid.java
@@ -7,24 +7,33 @@ import java.util.Random;
 
 //contain instances of individual boids
 public class Boid {
-    final int MAX_INSTANCES = 200; //max number of boids that can be active
+    final static int MAX_INSTANCES = 200; //max number of boids that can be active
     public static ArrayList<Boid> activeBoids;
-    public static Triangle_Shape boidShape = new Triangle_Shape();
     private int xVel, yVel; //velocities (in x-axis and y-axis)
     private int xPos, yPos; //x and y co-ordinates
+    private static Random r = new Random();
 
     public Boid() {
-
+        xPos = r.nextInt(AppPanel.PANEL_WIDTH);
+        yPos = r.nextInt(AppPanel.PANEL_HEIGHT);
+        xVel = r.nextInt(30);
+        yVel = r.nextInt(30);
     }
 
     public Boid(int _xPos, int _yPos) { //just declare co-ords
+        xPos = _xPos;
+        yPos = _yPos;
+        xVel = r.nextInt(5);
+        yVel = r.nextInt(5);
         activeBoids.add(this);
     }
 
     public Boid(int _xPos, int _yPos, int _xVel, int _yVel) { //declare co-ords and velocity
-        activeBoids.add(this);
+        xPos = _xPos;
+        yPos = _yPos;
         this.xVel = _xVel;
         this.yVel = _yVel;
+        activeBoids.add(this);
     }
 
     public void setxVel(int value) {
@@ -35,7 +44,29 @@ public class Boid {
         yVel = value;
     }
 
-    public Triangle_Shape[] spawn(int quantity) { //return array of Triangle_Shape obj -> co-ords of each point in the triangle for each Boid being spawned
+    public int getxVel() {
+        return xVel;
+    }
+
+    public int getyVel() {
+        return yVel;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void move(){
+        this.xPos += this.xVel;
+        this.yPos += this.yVel;
+    }
+
+
+  /*  public static Triangle_Shape[] spawn(int quantity) { //return array of Triangle_Shape obj -> co-ords of each point in the triangle for each Boid being spawned
         Random r = new Random();
         int x;
         int y;
@@ -49,25 +80,46 @@ public class Boid {
 
                 //calculate other points in triangle
                 int y1 = y + 16;
-                int x2 = x + 32;
+                int x2 = x + 16;
                 int y2 = y + 8;
 
-            triangles[i] = new Triangle_Shape(new Point2D.Double(x,y), new Point2D.Double(x,y1), new Point2D.Double(x2,y2)); //to here
+                triangles[i] = new Triangle_Shape(new Point2D.Double(x, y), new Point2D.Double(x, y1), new Point2D.Double(x2, y2)); //add boid shape to triangle array
+
+
+
             }
 
         } else return null;
 
 
         return triangles;
+    }*/
+
+    public static ArrayList<Boid> spawn(int quantity) {
+
+        int x;
+        int y;
+        ArrayList<Boid> boids = new ArrayList<>();
+
+
+        // if ((quantity + activeBoids.size()) < MAX_INSTANCES) {
+        for (int i = 0; i < quantity; i++) {
+            boids.add(new Boid());
+        }
+        //} else return null;
+
+
+        return boids;
     }
 }
 
-//shape of boid src-> https://www.delftstack.com/howto/java/java-draw-triangle/
-class Triangle_Shape extends Path2D.Double {
-    public Triangle_Shape(Point2D... points) {
-        moveTo(points[0].getX(), points[0].getY());
-        lineTo(points[1].getX(), points[1].getY());
-        lineTo(points[2].getX(), points[2].getY());
-        closePath();
-    }
-}
+
+////shape of boid src-> https://www.delftstack.com/howto/java/java-draw-triangle/
+//class Triangle_Shape extends Path2D.Double {
+//    public Triangle_Shape(Point2D... points) {
+//        moveTo(points[0].getX(), points[0].getY());
+//        lineTo(points[1].getX(), points[1].getY());
+//        lineTo(points[2].getX(), points[2].getY());
+//        closePath();
+//    }
+//}
